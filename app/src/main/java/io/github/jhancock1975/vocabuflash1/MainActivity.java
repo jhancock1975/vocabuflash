f707c94a85eb5e71c9764f8fe9ddbe7038ae2f1a
John Hancock <jhancock1975@gmail.com>
2019-12-20 12:23:18 -0500

    example code working

--- app/src/main/java/io/github/jhancock1975/vocabuflash1/MainActivity.java
+++ app/src/main/java/io/github/jhancock1975/vocabuflash1/MainActivity.java
@@ -1,39 +1,84 @@
 package io.github.jhancock1975.vocabuflash1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    private static String tag = "MainActivity";
+    private WordViewModel mWordViewModel;
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
+    /**
+     * According to https://www.vogella.com/tutorials/AndroidLogging/article.html
+     * Android wants us to check the build configuration every time we log anything
+     * so this function saves writing the if statement every time.
+     *
+     * @param tag the logging tag
+     * @param msg the log message
+     */
+    private void log(String tag, String msg){
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        log(tag, "starting up");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
             }
         });
-        imageView = findViewById(R.id.imageView2);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        /*imageView = findViewById(R.id.imageView2);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);*/
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setWords(words);
+            }
+        });
     }
 
     @Override
@@ -56,5 +101,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/io/github/jhancock1975/vocabuflash1/Word.java
@@ -0,0 +1,27 @@
+package io.github.jhancock1975.vocabuflash1;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private int mId;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+
+    public int getId(){return this.mId;}
+
+    public void setId(int id){ this.mId = id;}
+}
